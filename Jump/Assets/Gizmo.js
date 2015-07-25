01edda9935e99febb37e3efa1a23fadb812baf24
Ahmad Hussein <ahmad_h7@live.com>
2015-07-25 17:37:35 -0400

    solved merge issues

---
+++ Jump/Assets/Gizmo.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+using System.Collections.Generic;
+
+
+
+
+
+function Start () {
+public Color color=new Color(0.985f, 0.022f, 0.022f, 0.2f);
+public List targets=new List();
+public Vector3 size=new Vector3(1, 1, 0);
+}
+
+function Update () {
+
+}
